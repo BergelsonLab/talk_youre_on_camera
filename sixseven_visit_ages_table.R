10aa8874e1604657b16ad743a719f69e3ad50bec
Shannon Dailey <shannon.dailey@duke.edu>
2017-12-13 17:31:15 -0500

    made visit ages table into an R script

--- sixseven_visit_ages_table.R
+++ sixseven_visit_ages_table.R
@@ -1,45 +1,25 @@
----
-title: "sixseven_recording_ages"
-author: "sd"
-date: "9/15/2017"
-output: html_document
----
+# creates table of recordings ages for months 6-7
 
-```{r setup, echo=FALSE, results='hide', message=FALSE}
-#setwd("/Users/sd314/Desktop/github/blab/talk_youre_on_camera")
 library(dplyr)
+library(knitr)
 
-require("knitr")
-knitr::opts_chunk$set(cache=TRUE, fig.width = 20, fig.height=10, warning = F, strip.white = T)
-#opts_knit$set(root.dir = "/Users/sd314/Desktop/github/blab/talk_youre_on_camera")
-```
-
-``` {r reading in the data, echo=FALSE, results='hide', message=FALSE}
 visit_ages <- read.csv("data/sixseven_visit_ages.csv")
-```
 
-``` {r prep, echo=FALSE, results='hide', message=FALSE}
 visit_ages_6 <- visit_ages %>%
   filter(month == "6")
 
 visit_ages_7 <- visit_ages %>%
   filter(month == "7")
-```
-
-``` {r function, echo=FALSE}
 
 # MM;DD function
-
 mmdd <- function(mean,sd) {
   mos <- floor(mean/30.42)
   days <- round(mean-(mos*30.42),digits = 0)
-    step2 <- c("M=",mos,";",days,", SD=",round(sd,digits = 1)," days")
-    result <- paste(step2, sep="", collapse="") 
-    return(result)
+  step2 <- c("M=",mos,";",days,", SD=",round(sd,digits = 1)," days")
+  result <- paste(step2, sep="", collapse="") 
+  return(result)
 }
-```
 
-``` {r 6mo, echo=FALSE}
 # 6mo lab visit
 sixmo_lab = mmdd(mean(visit_ages_6$lab_age_days),sd(visit_ages_6$lab_age_days))
 
@@ -48,20 +28,17 @@ sixmo_audio = mmdd(mean(visit_ages_6$audio_age_days),sd(visit_ages_6$audio_age_d
 
 # 6mo video
 sixmo_video = mmdd(mean(visit_ages_6$video_age_days),sd(visit_ages_6$video_age_days))
-```
-
-``` {r 7mo, echo=FALSE}
 
 # 7mo audio
 sevenmo_audio = mmdd(mean(visit_ages_7$audio_age_days),sd(visit_ages_7$audio_age_days))
 
 # 7mo video
 sevenmo_video = mmdd(mean(visit_ages_7$video_age_days),sd(visit_ages_7$video_age_days))
-```
 
-``` {r table prep, echo=FALSE}
+
+# Table prep
 Month = c("6 months","7 months")
-  
+
 Video_recordings = c(sixmo_video,sevenmo_video)
 
 Audio_recordings = c(sixmo_audio,sevenmo_audio)
@@ -69,13 +46,16 @@ Audio_recordings = c(sixmo_audio,sevenmo_audio)
 Lab_visits = c(sixmo_lab,"NA")
 
 col_names = c("","Video recordings","Audio recordings","In-lab visits")
-  
-table_data <- data.frame(Month, Video_recordings, Audio_recordings, Lab_visits)
-```
+
+ages_table_data <- data.frame(Month, Video_recordings, Audio_recordings, Lab_visits)
 
 
-``` {r kable, echo=FALSE}
-kable(table_data, col.names = col_names, format = "markdown", padding = 2)
-```
+# Table
+sixseven_visit_ages_table <- kable(ages_table_data, col.names = col_names,
+                                   format = "markdown", padding = 2)
 
-
+# FOR RMARKDOWN
+# ```{r recording-ages-table, comment=F, message=F, hide = T, warning = F, echo = F}
+# source("sixseven_visit_ages_table.R")
+# sixseven_visit_ages_table
+# ```
